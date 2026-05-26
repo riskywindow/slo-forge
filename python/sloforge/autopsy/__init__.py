fe10bd316056d8d190f2dff6f42e2fe2d85e9b75
Rishi Vinodkumar <126040656+riskywindow@users.noreply.github.com>
2026-05-26 17:04:00 -0500

    Expose Autopsy counterfactual and minimization APIs

--- python/sloforge/autopsy/__init__.py
+++ python/sloforge/autopsy/__init__.py
@@ -2,7 +2,19 @@
 
 from .alignment import align_run, estimate_alignment
 from .comparison import compare_runs
+from .counterfactual import (
+    CounterfactualReplay,
+    CounterfactualScenario,
+    RemoveFault,
+    ReplaceResource,
+    ScaleRank,
+    ScaleResourceCurve,
+    ScenarioEvaluation,
+    replay_counterfactuals,
+    run_fabric_simulator,
+)
 from .diagnosis import diagnose, extract_signals
+from .minimize import MinimizationResult, minimize_run
 from .models import (
     AlignmentEstimate,
     AlignmentQuality,
@@ -30,16 +42,27 @@ __all__ = [
     "CausalHypothesis",
     "ClockSample",
     "CounterValue",
+    "CounterfactualReplay",
+    "CounterfactualScenario",
     "DiagnosisRecord",
     "DifferentialComparison",
     "EventType",
     "EvidenceRef",
     "FaultInterval",
+    "MinimizationResult",
+    "RemoveFault",
+    "ReplaceResource",
     "ResourceRef",
+    "ScaleRank",
+    "ScaleResourceCurve",
+    "ScenarioEvaluation",
     "SourceClock",
     "align_run",
     "compare_runs",
     "diagnose",
     "estimate_alignment",
     "extract_signals",
+    "minimize_run",
+    "replay_counterfactuals",
+    "run_fabric_simulator",
 ]
