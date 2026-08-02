cfafe4792eb647fa71f0fed5548d8b34ef6d203d
Rishi Vinodkumar <youremail@example.com>
2026-08-02 15:24:46 -0700

    Export artifact-backed Genesis campaigns

--- python/sloforge/genesis/evaluation_campaigns/__init__.py
+++ python/sloforge/genesis/evaluation_campaigns/__init__.py
@@ -7,11 +7,41 @@ from .autopsy import (
     run_autopsy_guided_campaign,
     validate_autopsy_guided_campaign,
 )
+from .lineage import (
+    H5LineageCampaignReport,
+    LineageScenario,
+    run_h5_lineage_campaign,
+    validate_h5_lineage_campaign,
+)
+from .unseen import (
+    H1CampaignConfiguration,
+    H1CampaignReport,
+    run_h1_unseen_campaign,
+    validate_h1_unseen_campaign,
+)
+from .whole_stack import (
+    WholeStackCampaignReport,
+    WholeStackValidationError,
+    run_whole_stack_campaign,
+    validate_whole_stack_campaign,
+)
 
 __all__ = [
     "AutopsyCampaignReport",
     "CampaignValidationError",
+    "H1CampaignConfiguration",
+    "H1CampaignReport",
+    "H5LineageCampaignReport",
+    "LineageScenario",
     "SearchStrategy",
+    "WholeStackCampaignReport",
+    "WholeStackValidationError",
     "run_autopsy_guided_campaign",
+    "run_h1_unseen_campaign",
+    "run_h5_lineage_campaign",
+    "run_whole_stack_campaign",
     "validate_autopsy_guided_campaign",
+    "validate_h1_unseen_campaign",
+    "validate_h5_lineage_campaign",
+    "validate_whole_stack_campaign",
 ]
